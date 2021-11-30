a77c7901ac20fe3276efd446233dad9e30d6cfa1
ninhnv <ninh.nguyen@mechmaster.io>
2021-11-30 10:31:31 +0700

    implement draw AoE on map (#14)
    * implement draw AoE on map
    
    * implement draw AoE on map
    
    * minor update

---
+++ Scripts/Utilities/Extension/UnitySerializedDictionary.cs
@@ -0,0 +1,35 @@
+﻿namespace GameFoundation.Scripts.Utilities.Extension
+{
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    public abstract class UnitySerializedDictionary<TKey, TValue> : Dictionary<TKey, TValue>, ISerializationCallbackReceiver
+    {
+        [SerializeField, HideInInspector]
+        private List<TKey> keyData = new List<TKey>();
+	
+        [SerializeField, HideInInspector]
+        private List<TValue> valueData = new List<TValue>();
+
+        void ISerializationCallbackReceiver.OnAfterDeserialize()
+        {
+            this.Clear();
+            for (int i = 0; i < this.keyData.Count && i < this.valueData.Count; i++)
+            {
+                this[this.keyData[i]] = this.valueData[i];
+            }
+        }
+
+        void ISerializationCallbackReceiver.OnBeforeSerialize()
+        {
+            this.keyData.Clear();
+            this.valueData.Clear();
+
+            foreach (var item in this)
+            {
+                this.keyData.Add(item.Key);
+                this.valueData.Add(item.Value);
+            }
+        }
+    }
+}
