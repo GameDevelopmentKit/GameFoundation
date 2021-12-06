62b8fa7e71fc5430bcc559b9664d1e06a2eae4ad
hoanghai1308 <90165017+hoanghai1308@users.noreply.github.com>
2021-12-06 10:48:34 +0700

    import play playlist (#21)

--- Scripts/Utilities/SoundManager.cs
+++ Scripts/Utilities/SoundManager.cs
@@ -6,12 +6,25 @@
     {
         void PlaySound(string name);
         void StopSound(string name);
+        void PlayPlayList(string playlist);
+        void StopPlaylist(string playlist, bool stopAll = false);
     }
 
     public class MasterMechSoundManager : IMechSoundManager
     {
         public void PlaySound(string name) { MasterAudio.PlaySound(name); }
         //To DO
-        public void StopSound(string name) { }
+        public void StopSound(string name)        { }
+        public void PlayPlayList(string playlist) { MasterAudio.StartPlaylist(playlist); }
+        public void StopPlaylist(string playlist, bool stopAll = false)
+        {
+            if (stopAll)
+            {
+                MasterAudio.StopAllPlaylists();
+                return;
+            }
+
+            MasterAudio.StopPlaylist(playlist);
+        }
     }
 }
