be67d3a7af5e9c307342d457b0100f3d622f53dc
tuhamm <tu.hoang@mechmaster.io>
2022-02-28 13:49:47 +0700

    Implement build info - CLIENT-1825

---
+++ Scripts/GameManager/GameInfoMono.cs
@@ -0,0 +1,20 @@
+﻿namespace GameFoundation.Scripts.GameManager
+{
+    using TMPro;
+    using UnityEngine;
+
+    [RequireComponent(typeof(TextMeshProUGUI))]
+    public class GameInfoMono : MonoBehaviour
+    {
+        private TextMeshProUGUI gameInfoText;
+
+        private void Start()
+        {
+            this.gameInfoText      = this.GetComponent<TextMeshProUGUI>();
+            this.gameInfoText.text = MechVersion.FullInfo;
+#if !DEVELOPMENT_BUILD && !UNITY_EDITOR
+            this.gameObject.SetActive(false);
+#endif
+        }
+    }
+}
