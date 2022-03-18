6771b9a93b4cb795008275efda8201dc6ba6ba37
chuongdp-mech <89625780+chuongdpna@users.noreply.github.com>
2022-03-18 14:45:38 +0700

    change define property of particle to list (#78)
    Co-authored-by: Mad Bean <97536490+unicornmadbean@users.noreply.github.com>

--- Scripts/Utilities/UIStuff/MechButton.cs
+++ Scripts/Utilities/UIStuff/MechButton.cs
@@ -1,63 +1,69 @@
 namespace GameFoundation.Scripts.Utilities.UIStuff
 {
-    using System;
+    using System.Collections.Generic;
     using UnityEngine;
-    using UnityEngine.Events;
     using UnityEngine.EventSystems;
     using UnityEngine.UI;
 
     [RequireComponent(typeof(Button))]
     [RequireComponent(typeof(DisallowMultipleComponent))]
-    public class MechButton : BaseMechSFX, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler, IDisposable
+    public class MechButton : BaseMechSFX, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler
     {
-        [SerializeField] private GameObject defaultParticle, appearParticle, hoverParticle, pressParticle;
-        private                  bool       isActiveHover;
-        private                  Button     btn;
-        public                   Button     Btn => this.btn;
+        [SerializeField] private List<GameObject> defaultParticles, appearParticles, hoverParticles, pressParticles;
+        private                  bool             isActiveHover;
+        private                  Button           btn;
+        public                   Button           Btn => this.btn;
 
-        private void Awake()
-        {
-            this.btn = this.GetComponent<Button>();
-        }
+        private void Awake() { this.btn = this.GetComponent<Button>(); }
 
         public void OnPointerClick(PointerEventData eventData)
         {
             this.OnPlaySfx();
-            
-            if (this.pressParticle == null) return;
-            this.pressParticle.SetActive(true);
+
+            if (this.pressParticles.Count <= 0) return;
+            foreach (var pressParticle in this.pressParticles)
+            {
+                pressParticle.SetActive(true);
+            }
         }
 
         public void OnPointerEnter(PointerEventData eventData)
         {
             if (!this.isActiveHover) return;
             if (!this.GetComponent<Button>().interactable) return;
-            if (this.hoverParticle == null) return;
-            this.hoverParticle.SetActive(true);
+            if (this.hoverParticles.Count <= 0) return;
+            foreach (var hoverParticle in this.hoverParticles)
+            {
+                hoverParticle.SetActive(true);
+            }
         }
 
         public void OnPointerExit(PointerEventData eventData)
         {
-            if (this.hoverParticle != null) 
-                this.hoverParticle.SetActive(false);
+            if (this.hoverParticles.Count <= 0) return;
+            foreach (var hoverParticle in this.hoverParticles)
+            {
+                hoverParticle.SetActive(true);
+            }
         }
 
         public void SetDefaultParticleActive(bool isActive)
         {
             this.isActiveHover = isActive;
-            if (this.defaultParticle == null) return;
-            this.defaultParticle.SetActive(isActive);
+            if (this.defaultParticles.Count <= 0) return;
+            foreach (var defaultParticle in this.defaultParticles)
+            {
+                defaultParticle.SetActive(true);
+            }
         }
-        
+
         public void EnableAppearParticle()
         {
-            if (this.appearParticle == null) return;
-            this.appearParticle.SetActive(true);
-        }
-        
-        public void Dispose()
-        {
-            
+            if (this.appearParticles.Count <= 0) return;
+            foreach (var appearParticle in this.appearParticles)
+            {
+                appearParticle.SetActive(true);
+            }
         }
     }
 }
