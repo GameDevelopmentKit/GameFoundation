81d0f736b8ebd37aad1b238abe4594eacc9f6dfc
chuongdp-mech <89625780+chuongdpna@users.noreply.github.com>
2022-03-18 11:04:15 +0700

    update mechbutton with show press/hover/appear/default particle function (#75)
    Co-authored-by: Mad Bean <97536490+unicornmadbean@users.noreply.github.com>

--- Scripts/Utilities/UIStuff/MechButton.cs
+++ Scripts/Utilities/UIStuff/MechButton.cs
@@ -2,13 +2,46 @@ namespace GameFoundation.Scripts.Utilities.Extension
 {
     using UnityEngine;
     using UnityEngine.EventSystems;
+    using UnityEngine.UI;
 
-    
-    public class MechButton : BaseMechSFX,IPointerClickHandler
+    public class MechButton : BaseMechSFX, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler
     {
+        [SerializeField] private GameObject defaultParticle, appearParticle, hoverParticle, pressParticle;
+        private                  bool       isActiveHover;
+
         public void OnPointerClick(PointerEventData eventData)
         {
             this.OnPlaySfx();
+            
+            if (this.pressParticle == null) return;
+            this.pressParticle.SetActive(true);
+        }
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            if (!this.isActiveHover) return;
+            if (!this.GetComponent<Button>().interactable) return;
+            if (this.hoverParticle == null) return;
+            this.hoverParticle.SetActive(true);
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            if (this.hoverParticle != null) 
+                this.hoverParticle.SetActive(false);
+        }
+
+        public void SetDefaultParticleActive(bool isActive)
+        {
+            this.isActiveHover = isActive;
+            if (this.defaultParticle == null) return;
+            this.defaultParticle.SetActive(isActive);
+        }
+        
+        public void EnableAppearParticle()
+        {
+            if (this.appearParticle == null) return;
+            this.appearParticle.SetActive(true);
         }
     }
 }
