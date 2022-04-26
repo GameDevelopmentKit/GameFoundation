bba6cc94910a9a852d4fc6543bc81a5ef4da9da4
chuongdp-mech <89625780+chuongdpna@users.noreply.github.com>
2022-04-26 14:22:18 +0700

    Chuongdp/implement/expose shuffle function (#105)
    * remove shuffle function in ListExtension
    
    * rename shuffle function in gamefoundation
    
    Co-authored-by: chuongdp <chuong.dau@mechmaster.io>

--- Scripts/Utilities/Extension/ListExtension.cs
+++ Scripts/Utilities/Extension/ListExtension.cs
@@ -33,9 +33,9 @@
 
         public static T PickRandom<T>(this IEnumerable<T> source) { return source.PickRandom(1).Single(); }
 
-        public static IEnumerable<T> PickRandom<T>(this IEnumerable<T> source, int count) { return source.Shuffle().Take(count); }
+        public static IEnumerable<T> PickRandom<T>(this IEnumerable<T> source, int count) { return source.ShuffleSource().Take(count); }
 
-        public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> source)
+        public static IEnumerable<T> ShuffleSource<T>(this IEnumerable<T> source)
         {
             return source.OrderBy(x => Guid.NewGuid());
         }
