77a6851d7f52d3851c1d841da114a72644926f66
hoanghai1308 <90165017+hoanghai1308@users.noreply.github.com>
2022-07-04 09:58:55 +0700

    hot fix (#123)

--- Scripts/BlueprintFlow/BlueprintControlFlow/DebugBlueprintConfig.cs
+++ Scripts/BlueprintFlow/BlueprintControlFlow/DebugBlueprintConfig.cs
@@ -1,12 +1,14 @@
 namespace GameFoundation.Scripts.BlueprintFlow.BlueprintControlFlow
 {
     using UnityEngine;
-
     public class DebugBlueprintConfig : BlueprintConfig
     {
+#if UNITY_EDITOR
         public override string BlueprintZipFilepath => "Assets/Resources/BlueprintData/Blueprints_v1.0.zip";
+#endif
 #if UNITY_ANDROID &&!UNITY_EDITOR
-        public override string BlueprintZipFilepath => $"{Application.persistentDataPath}/Blueprints_v1.0.zip";
+ static string persistentDataPath = Application.persistentDataPath;
+        public override string BlueprintZipFilepath => $"{persistentDataPath}/Blueprints_v1.0.zip";
 #endif
     }
 }
