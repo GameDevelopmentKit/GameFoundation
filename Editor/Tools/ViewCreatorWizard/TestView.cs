46b9508c5edf5d301cd495aa8ae119c48ae1b2e4
phuongdmp7 <phuongdmp7@gmail.com>
2022-12-07 22:03:29 +0700

    complete tool

--- Editor/Tools/ViewCreatorWizard/TestView.cs
+++
@@ -1,9 +0,0 @@
-namespace GameFoundation.Editor.Tools.ViewCreatorWizard
-{
-    using UnityEngine;
-
-    public class TestView : MonoBehaviour
-    {
-        
-    }
-}
--- Editor/Tools/ViewCreatorWizard/ViewCreatorWizard.cs
+++ Editor/Tools/ViewCreatorWizard/ViewCreatorWizard.cs
@@ -5,42 +5,44 @@ namespace GameFoundation.Editor.Tools.ViewCreatorWizard
     using System.IO;
     using System.Linq;
     using System.Text.RegularExpressions;
+    using Newtonsoft.Json;
     using UnityEditor;
     using UnityEngine;
     using UnityEngine.UIElements;
     using Button = UnityEngine.UIElements.Button;
+    using Object = System.Object;
     using Toggle = UnityEngine.UIElements.Toggle;
 
     public partial class ViewCreatorWizard : EditorWindow
     {
-        private const string LOG_TAG             = "GF ViewCreatorWizard: ";
-        private const string FOLDER_PATH_DEFAULT = "Assets";
-        private const string UI_BASE_POPUP_PATH  = "Assets/GameFoundation/Prefabs/CommonUIPrefab/UIBasePopup.prefab";
-        private const string UI_BASE_SCREEN_PATH = "Assets/GameFoundation/Prefabs/CommonUIPrefab/UIBaseScreen.prefab";
+        private const string LOG_TAG              = "GF ViewCreatorWizard: ";
+        private const string FOLDER_PATH_DEFAULT  = "Assets";
+        private const string UI_BASE_POPUP_PATH   = "Assets/GameFoundation/Prefabs/CommonUIPrefab/UIBasePopup.prefab";
+        private const string UI_BASE_SCREEN_PATH  = "Assets/GameFoundation/Prefabs/CommonUIPrefab/UIBaseScreen.prefab";
+        private const string TASK_CREATE_VIEW_KEY = "GF_TASK_CREATE_VIEW_KEY";
 
-        private (bool hasTask, string filePath) hasGeneratePrefabTask;
+        private TaskCreateView taskCreateView;
 
-        #region Variables
+        #region UITookit element
 
         private DropdownField dropdownSettingType;
-        private TextField     inputSettingName;
-        private TextField     inputSettingPath;
-        private Button        btnSettingLocation;
         private Toggle        toggleSettingHasModel;
+        private TextField     inputSettingName;
+        private TextField     inputSettingScriptPath;
+        private Button        btnSettingScriptLocation;
+        private TextField     inputSettingPrefabPath;
+        private Button        btnSettingPrefabLocation;
 
-        private VisualElement modelContainer;
-        private TextField     inputModelName;
-        private TextField     inputModelPath;
-        private TextField     inputViewName;
-        private TextField     inputViewPath;
-        private TextField     inputPresenterName;
-        private TextField     inputPresenterPath;
-        private Button        btnGenerate;
+        private TextField inputModelName;
+        private TextField inputViewName;
+        private TextField inputPresenterName;
+        private Button    btnGenerate;
 
         #endregion
 
         #region Open Window
 
+        [MenuItem("GDK/View Creator Wizard")]
         [MenuItem("Assets/GameFoundation/View Creator Wizard")]
         public static void OpenWindow()
         {
@@ -70,39 +72,39 @@ namespace GameFoundation.Editor.Tools.ViewCreatorWizard
 
         #endregion
 
+        #region GUI Callback
+
         public void CreateGUI()
         {
-            Debug.Log("CreateGUI ");
-            // this.hasGeneratePrefabTask = (false,null);
+            this.taskCreateView = EditorPrefs.HasKey(TASK_CREATE_VIEW_KEY) ? JsonConvert.DeserializeObject<TaskCreateView>(EditorPrefs.GetString(TASK_CREATE_VIEW_KEY)) : new TaskCreateView();
+            Debug.Log("CreateGUI " + this.taskCreateView.PrefabAssetPath);
 
             var root       = this.rootVisualElement;
             var visualTree = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/GameFoundation/Editor/Tools/ViewCreatorWizard/ViewCreatorWizard.uxml");
 
             root.Add(visualTree.Instantiate());
 
-            // Element Init
-
             #region init element reference
 
-            this.dropdownSettingType   = root.Q<DropdownField>("dropdownSettingType");
-            this.inputSettingName      = root.Q<TextField>("inputSettingName");
-            this.inputSettingPath      = root.Q<TextField>("inputSettingPath");
-            this.btnSettingLocation    = root.Q<Button>("btnSettingLocation");
-            this.toggleSettingHasModel = root.Q<Toggle>("toggleSettingHasModel");
-            this.modelContainer        = root.Q<VisualElement>("modelContainer");
-            this.inputModelName        = root.Q<TextField>("inputModelName");
-            this.inputModelPath        = root.Q<TextField>("inputModelPath");
-            this.inputViewName         = root.Q<TextField>("inputViewName");
-            this.inputViewPath         = root.Q<TextField>("inputViewPath");
-            this.inputPresenterName    = root.Q<TextField>("inputPresenterName");
-            this.inputPresenterPath    = root.Q<TextField>("inputPresenterPath");
-            this.btnGenerate           = root.Q<Button>("btnGenerate");
+            this.dropdownSettingType      = root.Q<DropdownField>("dropdownSettingType");
+            this.toggleSettingHasModel    = root.Q<Toggle>("toggleSettingHasModel");
+            this.inputSettingName         = root.Q<TextField>("inputSettingName");
+            this.inputSettingScriptPath   = root.Q<TextField>("inputSettingScriptPath");
+            this.btnSettingScriptLocation = root.Q<Button>("btnSettingScriptLocation");
+            this.inputSettingPrefabPath   = root.Q<TextField>("inputSettingPrefabPath");
+            this.btnSettingPrefabLocation = root.Q<Button>("btnSettingPrefabLocation");
+
+            this.inputModelName     = root.Q<TextField>("inputModelName");
+            this.inputViewName      = root.Q<TextField>("inputViewName");
+            this.inputPresenterName = root.Q<TextField>("inputPresenterName");
+            this.btnGenerate        = root.Q<Button>("btnGenerate");
 
             #endregion
 
             this.InitViewTypeSetting();
             this.InitViewNameSetting();
-            this.InitViewPathSetting();
+            this.InitViewPathSetting(this.inputSettingScriptPath, this.btnSettingScriptLocation);
+            this.InitViewPathSetting(this.inputSettingPrefabPath, this.btnSettingPrefabLocation);
             this.InitHasModelSetting();
             this.InitGenerateButton();
 
@@ -111,72 +113,52 @@ namespace GameFoundation.Editor.Tools.ViewCreatorWizard
 
         private void OnGUI()
         {
-            Debug.Log("this.hasGeneratePrefabTask.hasTask "+this.hasGeneratePrefabTask.hasTask);
-            if (!EditorApplication.isCompiling && this.hasGeneratePrefabTask.hasTask)
+            if (EditorApplication.isCompiling || this.taskCreateView == null || this.taskCreateView.IsTaskComplete) return;
+
+            this.GeneratePrefab();
+            EditorPrefs.DeleteKey(TASK_CREATE_VIEW_KEY);
+            this.taskCreateView = new TaskCreateView();
+        }
+
+        #endregion
+
+        #region Generate Script and Prefab
+
+        private void GeneratePrefab()
+        {
+            Debug.Log("Start GeneratePrefab " + this.taskCreateView.PrefabAssetPath);
+            var scriptType = GetTypeFromAllAssemblies(this.taskCreateView.TypeFullName);
+            if (scriptType == null) return;
+
+            GameObject objSource;
+            if (this.taskCreateView.ViewType == ViewType.Item)
             {
-                Debug.Log("action");
-                this.GeneratePrefab(this.hasGeneratePrefabTask.filePath);
-                this.hasGeneratePrefabTask = (false,null);
+                objSource = new GameObject();
+                objSource.AddComponent<RectTransform>();
             }
-        }
-
-        #region Init Callback
-
-        private void InitGenerateButton()
-        {
-            this.btnGenerate.clicked += () =>
+            else
             {
-                if (!Enum.TryParse(this.dropdownSettingType.value, out ViewType type))
-                {
-                    Debug.LogError(LOG_TAG + "Invalid type");
-                    return;
-                }
+                var prefabPath     = this.taskCreateView.ViewType == ViewType.Popup ? UI_BASE_POPUP_PATH : UI_BASE_SCREEN_PATH;
+                var originalPrefab = (GameObject)AssetDatabase.LoadAssetAtPath(prefabPath, typeof(GameObject));
+                objSource = PrefabUtility.InstantiatePrefab(originalPrefab) as GameObject;
+            }
 
-                var sTemplate = type switch
-                {
-                    ViewType.Item => ITEM_VIEW_TEMPLATE,
-                    ViewType.Popup => this.toggleSettingHasModel.value ? POPUP_VIEW_TEMPLATE : POPUP_VIEW_NON_MODEL_TEMPLATE,
-                    ViewType.Screen => this.toggleSettingHasModel.value ? SCREEN_VIEW_TEMPLATE : SCREEN_VIEW_NON_MODEL_TEMPLATE,
-                    _ => throw new ArgumentOutOfRangeException()
-                };
-                var viewName                                           = this.inputViewName.value;
-                var rootDirectoryPath                                  = this.inputSettingPath.value;
-                if (rootDirectoryPath.EndsWith('/')) rootDirectoryPath = rootDirectoryPath.Substring(0, rootDirectoryPath.Length - 1);
-                var nameSpacePath                                      = rootDirectoryPath.Replace(FOLDER_PATH_DEFAULT, "");
-                var genScriptDirectoryPath                             = Application.dataPath + "/" + nameSpacePath;
-                var genScriptPath                                      = genScriptDirectoryPath + "/" + viewName + ".cs";
-
-                sTemplate = sTemplate.Replace("X_NAME_SPACE", string.IsNullOrWhiteSpace(nameSpacePath) ? "A" : nameSpacePath);
-                sTemplate = sTemplate.Replace("X_MODEL_NAME", this.inputModelName.value);
-                sTemplate = sTemplate.Replace("X_VIEW_NAME", viewName);
-                sTemplate = sTemplate.Replace("X_PRESENTER_NAME", this.inputPresenterName.value);
-
-                if (TryGenerateScript(genScriptPath, genScriptDirectoryPath, sTemplate))
-                {
-                    this.hasGeneratePrefabTask = (true,$"{rootDirectoryPath}/{viewName}.prefab");
-                    Debug.Log("set callback");
-
-                }
-            };
+            Debug.Log("Middle GeneratePrefab this.taskCreateView.TypeFullName: " + this.taskCreateView.TypeFullName);
+            objSource.AddComponent(scriptType);
+            var prefabVariant = PrefabUtility.SaveAsPrefabAsset(objSource, this.taskCreateView.PrefabAssetPath);
+           
+            DestroyImmediate(objSource);
+            Debug.Log("End GeneratePrefab ");
+            EditorApplication.RepaintHierarchyWindow();
+            Debug.Log($"<color=green>Create prefab success! Save at: {this.taskCreateView.PrefabAssetPath}</color>");
+            this.Close();
         }
-        
-        void GeneratePrefab(string filePath)
-        {
-            var prefabPath = UI_BASE_POPUP_PATH;
 
-            var originalPrefab = (GameObject)AssetDatabase.LoadAssetAtPath(prefabPath, typeof(GameObject));
-            var objSource      = PrefabUtility.InstantiatePrefab(originalPrefab) as GameObject;
-            var scriptType     = Type.GetType("NotificationPopupUIView" + ",Assembly-CSharp");
-            var prefabVariant  = PrefabUtility.SaveAsPrefabAsset(objSource,filePath );
-            // prefabVariant.AddComponent(scriptType);
-            prefabVariant.AddComponent<TestView>();
-        }
-        
         private static bool TryGenerateScript(string genScriptPath, string genScriptDirectoryPath, string sTemplate)
         {
             if (File.Exists(genScriptPath))
             {
-                Debug.LogError(LOG_TAG + "File Exist!");
+                Debug.LogError(LOG_TAG + "File Exist! " + genScriptPath);
                 return false;
             }
 
@@ -207,35 +189,95 @@ namespace GameFoundation.Editor.Tools.ViewCreatorWizard
             return true;
         }
 
+        #endregion
+
+        #region Init GUI Element Action
+
+        private void InitGenerateButton()
+        {
+            this.btnGenerate.clicked += () =>
+            {
+                if (!Enum.TryParse(this.dropdownSettingType.value, out ViewType type))
+                {
+                    Debug.LogError(LOG_TAG + "Invalid type");
+                    return;
+                }
+
+                var sTemplate = type switch
+                {
+                    ViewType.Item => ITEM_VIEW_TEMPLATE,
+                    ViewType.Popup => this.toggleSettingHasModel.value ? POPUP_VIEW_TEMPLATE : POPUP_VIEW_NON_MODEL_TEMPLATE,
+                    ViewType.Screen => this.toggleSettingHasModel.value ? SCREEN_VIEW_TEMPLATE : SCREEN_VIEW_NON_MODEL_TEMPLATE,
+                    _ => throw new ArgumentOutOfRangeException()
+                };
+
+                var viewName = this.inputViewName.value; // PlayerItemView
+
+                var projectRelativeScriptPath                                          = this.inputSettingScriptPath.value; // Assets or Assets/Phuong/Doan/
+                if (projectRelativeScriptPath.EndsWith('/')) projectRelativeScriptPath = projectRelativeScriptPath[..^1]; // Assets or Assets/Phuong/Doan
+
+                var projectRelativePrefabPath                                          = this.inputSettingPrefabPath.value; // Assets or Assets/Phuong/Doan/
+                if (projectRelativePrefabPath.EndsWith('/')) projectRelativePrefabPath = projectRelativePrefabPath[..^1]; // Assets or Assets/Phuong/Doan
+
+                var nameSpaceScriptPath = projectRelativeScriptPath.Replace("Assets/", ""); // Assets or Phuong/Doan
+                nameSpaceScriptPath = nameSpaceScriptPath.Replace("Assets", ""); // "" or Phuong/Doan
+                var genScriptPath =
+                    Application.dataPath + (string.IsNullOrWhiteSpace(nameSpaceScriptPath) ? "" : ("/" + nameSpaceScriptPath)); // C:UnityProject/Assets or C:UnityProject/Assets/Phuong/Doan
+                var genScriptFullPath = genScriptPath + "/" + viewName + ".cs"; // C:UnityProject/Assets/PlayerItemView.cs or C:UnityProject/Assets/Phuong/Doan/PlayerItemView.cs
+                var nameSpace         = string.IsNullOrWhiteSpace(nameSpaceScriptPath) ? "A" : nameSpaceScriptPath.Replace("/", "."); // A or Phuong.Doan
+                nameSpace = nameSpace.Replace(" ", "_");
+
+                sTemplate = sTemplate.Replace("X_NAME_SPACE", nameSpace);
+                sTemplate = sTemplate.Replace("X_MODEL_NAME", this.inputModelName.value);
+                sTemplate = sTemplate.Replace("X_VIEW_NAME", viewName);
+                sTemplate = sTemplate.Replace("X_PRESENTER_NAME", this.inputPresenterName.value);
+
+
+                Debug.Log("InitGenerateButton " + genScriptFullPath + " |genScriptPath " + genScriptPath);
+                if (TryGenerateScript(genScriptFullPath, genScriptPath, sTemplate))
+                {
+                    Debug.Log($"<color=green>Create script success! Save at: {genScriptFullPath}</color>");
+                    var serializeObject = JsonConvert.SerializeObject(new TaskCreateView()
+                    {
+                        IsTaskComplete  = false,
+                        PrefabAssetPath = $"{projectRelativePrefabPath}/{viewName}.prefab",
+                        TypeFullName    = $"{nameSpace}.{viewName}",
+                        ViewType        = type
+                    });
+                    Debug.Log("InitGenerateButton serializeObject: " + serializeObject);
+                    EditorPrefs.SetString(TASK_CREATE_VIEW_KEY, serializeObject);
+                }
+            };
+        }
+
         private void InitHasModelSetting()
         {
             this.toggleSettingHasModel.RegisterValueChangedCallback(evt =>
             {
                 if (!Enum.TryParse(this.dropdownSettingType.value, out ViewType type)) return;
                 if (type == ViewType.Item && !evt.newValue) return;
-                this.modelContainer.style.display = evt.newValue ? DisplayStyle.Flex : DisplayStyle.None;
+                this.inputModelName.style.display = evt.newValue ? DisplayStyle.Flex : DisplayStyle.None;
             });
         }
 
-        private void InitViewPathSetting()
+        private void InitViewPathSetting(TextField inputPath, Button btnLocation)
         {
-            this.inputSettingPath.RegisterValueChangedCallback(evt =>
+            inputPath.RegisterValueChangedCallback(evt =>
             {
                 if (string.IsNullOrWhiteSpace(evt.newValue))
                 {
-                    this.inputSettingPath.value = FOLDER_PATH_DEFAULT;
-                    return;
+                    inputPath.value = FOLDER_PATH_DEFAULT;
                 }
 
-                this.UpdateAllPath();
+                inputPath.value = inputPath.value.Trim();
             });
 
-            this.btnSettingLocation.clicked += () =>
+            btnLocation.clicked += () =>
             {
                 var path = EditorUtility.OpenFolderPanel("Choose path", FOLDER_PATH_DEFAULT, "");
                 if (path.Length != 0 && path.Contains("Assets"))
                 {
-                    this.inputSettingPath.value = FileUtil.GetProjectRelativePath(path);
+                    inputPath.value = FileUtil.GetProjectRelativePath(path);
                 }
                 else
                 {
@@ -287,26 +329,21 @@ namespace GameFoundation.Editor.Tools.ViewCreatorWizard
             this.inputPresenterName.value = this.inputSettingName.value + this.dropdownSettingType.value + "Presenter";
         }
 
-        private void UpdateAllPath()
-        {
-            this.inputModelPath.value     = this.inputSettingPath.value;
-            this.inputViewPath.value      = this.inputSettingPath.value;
-            this.inputPresenterPath.value = this.inputSettingPath.value;
-        }
-
         private void SetDefaultValue()
         {
             var target = Selection.activeObject;
             var path   = target == null ? FOLDER_PATH_DEFAULT : AssetDatabase.GetAssetPath(target);
 
-            this.inputSettingPath.value    = path;
-            this.inputSettingName.value    = "Temp";
-            this.dropdownSettingType.value = this.dropdownSettingType.choices[0];
+            this.inputSettingScriptPath.value = path;
+            this.inputSettingPrefabPath.value = path;
+            this.inputSettingName.value       = "Temp";
+            this.dropdownSettingType.value    = this.dropdownSettingType.choices[0];
         }
 
-        #endregion
-
-        #region Helpers
+        private static Type GetTypeFromAllAssemblies(string typeFullName)
+        {
+            return AppDomain.CurrentDomain.GetAssemblies().Select(assembly => assembly.GetType(typeFullName)).FirstOrDefault(type => type != null);
+        }
 
         private static readonly Regex  SWhitespace = new Regex(@"\s+");
         private static          string RemoveWhitespace(string input) { return SWhitespace.Replace(input, ""); }
@@ -319,5 +356,15 @@ namespace GameFoundation.Editor.Tools.ViewCreatorWizard
         Item,
         Popup,
         Screen
+    }
+
+    public class TaskCreateView
+    {
+        public bool     IsTaskComplete;
+        public ViewType ViewType;
+        public string   TypeFullName;
+        public string   PrefabAssetPath;
+
+        public TaskCreateView() { this.IsTaskComplete = true; }
     }
 }
