52fd0f752158cda509d501b9aeb4c6568a0e290c
ninhnv <ninhnv15@gmail.com>
2022-12-12 09:49:48 +0700

    update formula for portrait screen and landscape screen

--- Scripts/UIModule/Utilities/ScaleScreenRatio.cs
+++ Scripts/UIModule/Utilities/ScaleScreenRatio.cs
@@ -9,19 +9,20 @@ namespace GameFoundation.Scripts.UIModule.Utilities
         /// <summary>
         /// Ratio of wide screen = 16/9, approx. 1.8
         /// </summary>
-        private const float WideScreenRatio = 1.8f;
-        private void Awake()
-        {
-            this.SetCanvasScaler();
-        }
+        private const float LandscapeStandardScreenRatio = 1.8f;
+
+        private const float PortraitStandardScreenRatio = 0.56f;
+        private       void  Awake() { this.SetCanvasScaler(); }
 
         private void SetCanvasScaler()
         {
             // if current screen ratio > WideScreenRatio, it will be the long screen the need keep height and scale width, and vice versa 
 #if UNITY_EDITOR
-            this.GetComponent<CanvasScaler>().matchWidthOrHeight = Screen.width* 1.0f / Screen.height >= WideScreenRatio ? 1 : 0;
+            var standardScreenRatio = Screen.width > Screen.height ? LandscapeStandardScreenRatio : PortraitStandardScreenRatio;
+            this.GetComponent<CanvasScaler>().matchWidthOrHeight = Screen.width * 1.0f / Screen.height >= standardScreenRatio ? 1 : 0;
 #else
-            this.GetComponent<CanvasScaler>().matchWidthOrHeight = Screen.currentResolution.width * 1.0f / Screen.currentResolution.height >= WideScreenRatio ? 1 : 0;
+            var standardScreenRatio = Screen.width > Screen.height ? LandscapeStandardScreenRatio : PortraitStandardScreenRatio;
+            this.GetComponent<CanvasScaler>().matchWidthOrHeight = Screen.currentResolution.width * 1.0f / Screen.currentResolution.height >= standardScreenRatio ? 1 : 0;
 #endif
         }
     }
