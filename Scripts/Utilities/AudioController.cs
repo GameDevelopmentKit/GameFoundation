58aa854a343a221af7a6f4eabd86446046bb49a1
hoanghai1308 <hoangthanhhai1308@gmail.com>
2023-02-23 15:52:29 +0700

    Update sound

---
+++ Scripts/Utilities/AudioController.cs
@@ -0,0 +1,10 @@
+﻿namespace GameFoundation.Scripts.Utilities
+{
+    using DarkTonic.MasterAudio;
+    using UnityEngine;
+
+    public class AudioController : MonoBehaviour
+    {
+        public MasterAudio masterAudio => this.GetComponent<MasterAudio>();
+    }
+}
