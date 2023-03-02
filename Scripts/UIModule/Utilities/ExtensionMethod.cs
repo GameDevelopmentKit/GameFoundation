dae18396288f1de49bb8d38be0023eb7aff0d51c
Hoàng Anh Tú <tuha263@gmail.com>
2023-03-02 19:37:14 +0700

    Revert "Update sound (#31)"
    This reverts commit 300c97a8732367d0dbb1f04d3c74fdac7b2f515b.

--- Scripts/UIModule/Utilities/ExtensionMethod.cs
+++ Scripts/UIModule/Utilities/ExtensionMethod.cs
@@ -12,7 +12,6 @@ namespace GameFoundation.Scripts.UIModule.Utilities
     using UnityEngine;
     using UnityEngine.UI;
     using Zenject;
-    using Button = UnityEngine.UI.Button;
 
     public static class ExtensionMethod
     {
