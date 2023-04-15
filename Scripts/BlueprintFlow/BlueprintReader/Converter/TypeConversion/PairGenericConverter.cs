180fd707c590158a83f2e47dbd5d249fdc0513d3
zommpie-duymai <buiduymai2k@gmail.com>
2023-04-15 10:48:26 +0700

    convert to string pair converter

--- Scripts/BlueprintFlow/BlueprintReader/Converter/TypeConversion/PairGenericConverter.cs
+++ Scripts/BlueprintFlow/BlueprintReader/Converter/TypeConversion/PairGenericConverter.cs
@@ -29,5 +29,18 @@ namespace BlueprintFlow.BlueprintReader.Converter.TypeConversion
 
             return null;
         }
+
+        public override string ConvertToString(object value, Type typeInfo)
+        {
+            var item1Type   = typeInfo.GetGenericArguments()[0];
+            var item2Type   = typeInfo.GetGenericArguments()[1];
+            var keyPairType = typeof(Tuple<,,>).MakeGenericType(item1Type, item2Type);
+
+            var item1Converter = CsvHelper.TypeConverterCache.GetConverter(item1Type);
+            var item2Converter = CsvHelper.TypeConverterCache.GetConverter(item2Type);
+            return string.Join(this.delimiterPair,
+                item1Converter.ConvertToString(keyPairType.GetProperty("Item1").GetValue(value), item1Type),
+                item2Converter.ConvertToString(keyPairType.GetProperty("Item2").GetValue(value), item2Type));
+        }
     }
 }
