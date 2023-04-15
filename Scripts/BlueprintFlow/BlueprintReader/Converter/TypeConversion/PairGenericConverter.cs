f8ad185cebd6f201859f52477ff95fd37dbe47c5
DuyMaiBui <103514604+DuyMaiBui@users.noreply.github.com>
2023-04-15 14:12:53 +0700

    Duymai/develop/convert to string pair converter (#49)
    * convert to string pair converter
    
    * change pair converter delimiter
    
    ---------
    
    Co-authored-by: zommpie-duymai <buiduymai2k@gmail.com>

--- Scripts/BlueprintFlow/BlueprintReader/Converter/TypeConversion/PairGenericConverter.cs
+++ Scripts/BlueprintFlow/BlueprintReader/Converter/TypeConversion/PairGenericConverter.cs
@@ -6,7 +6,7 @@ namespace BlueprintFlow.BlueprintReader.Converter.TypeConversion
     public class PairGenericConverter : DefaultTypeConverter
     {
         private readonly char delimiterPair;
-        public PairGenericConverter(char delimiterPair = '-') { this.delimiterPair = delimiterPair; }
+        public PairGenericConverter(char delimiterPair = ':') { this.delimiterPair = delimiterPair; }
 
 
         public override object ConvertFromString(string text, Type typeInfo)
@@ -28,6 +28,19 @@ namespace BlueprintFlow.BlueprintReader.Converter.TypeConversion
             }
 
             return null;
+        }
+
+        public override string ConvertToString(object value, Type typeInfo)
+        {
+            var item1Type   = typeInfo.GetGenericArguments()[0];
+            var item2Type   = typeInfo.GetGenericArguments()[1];
+            var keyPairType = typeof(Tuple<,>).MakeGenericType(item1Type, item2Type);
+
+            var item1Converter = CsvHelper.TypeConverterCache.GetConverter(item1Type);
+            var item2Converter = CsvHelper.TypeConverterCache.GetConverter(item2Type);
+            return string.Join(this.delimiterPair,
+                item1Converter.ConvertToString(keyPairType.GetProperty("Item1").GetValue(value), item1Type),
+                item2Converter.ConvertToString(keyPairType.GetProperty("Item2").GetValue(value), item2Type));
         }
     }
 }
