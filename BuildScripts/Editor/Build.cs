6ebdb7d239ce041520db4c4fb482fd58f521fd2f
phuongdmp7 <phuongdmp7@gmail.com>
2023-06-09 13:53:52 +0700

    comment all setting

--- BuildScripts/Editor/Build.cs
+++ BuildScripts/Editor/Build.cs
@@ -293,24 +293,24 @@ public static class Build
 #endif
                 break;
             case BuildTarget.WebGL:
-                PlayerSettings.SetManagedStrippingLevel(platform.BuildTargetGroup, ManagedStrippingLevel.High);
-                PlayerSettings.WebGL.decompressionFallback = true;
-                PlayerSettings.WebGL.compressionFormat     = WebGLCompressionFormat.Disabled; // Disable compression for FBInstant game
-                PlayerSettings.runInBackground             = false;
-                PlayerSettings.WebGL.powerPreference       = WebGLPowerPreference.HighPerformance;
-                PlayerSettings.WebGL.dataCaching           = true;
-                PlayerSettings.WebGL.exceptionSupport      = WebGLExceptionSupport.FullWithoutStacktrace;
-#if UNITY_2022_1_OR_NEWER
-                PlayerSettings.WebGL.initialMemorySize = 32;
-                PlayerSettings.SetIl2CppCodeGeneration(NamedBuildTarget.WebGL, il2CppCodeGeneration);
-#if FB_INSTANT
-                PlayerSettings.WebGL.showDiagnostics  = false;
-                PlayerSettings.WebGL.exceptionSupport = WebGLExceptionSupport.None;
-#else
-                PlayerSettings.WebGL.showDiagnostics = true;
-#endif // FB_INSTANT
-
-#endif // UNITY_2022_1_OR_NEWER
+//                 PlayerSettings.SetManagedStrippingLevel(platform.BuildTargetGroup, ManagedStrippingLevel.High);
+//                 PlayerSettings.WebGL.decompressionFallback = true;
+//                 PlayerSettings.WebGL.compressionFormat     = WebGLCompressionFormat.Disabled; // Disable compression for FBInstant game
+//                 PlayerSettings.runInBackground             = false;
+//                 PlayerSettings.WebGL.powerPreference       = WebGLPowerPreference.HighPerformance;
+//                 PlayerSettings.WebGL.dataCaching           = true;
+//                 PlayerSettings.WebGL.exceptionSupport      = WebGLExceptionSupport.FullWithoutStacktrace;
+// #if UNITY_2022_1_OR_NEWER
+//                 PlayerSettings.WebGL.initialMemorySize = 32;
+//                 PlayerSettings.SetIl2CppCodeGeneration(NamedBuildTarget.WebGL, il2CppCodeGeneration);
+// #if FB_INSTANT
+//                 PlayerSettings.WebGL.showDiagnostics  = false;
+//                 PlayerSettings.WebGL.exceptionSupport = WebGLExceptionSupport.None;
+// #else
+//                 PlayerSettings.WebGL.showDiagnostics = true;
+// #endif // FB_INSTANT
+//
+// #endif // UNITY_2022_1_OR_NEWER
                 break;
             default:
                 throw new ArgumentOutOfRangeException();
