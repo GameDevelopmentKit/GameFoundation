8d60a563830658c37867f8a7384a28d3f94566fb
phuongdmp7 <phuongdmp7@gmail.com>
2023-06-09 16:22:50 +0700

    set strip to low

--- BuildScripts/Editor/Build.cs
+++ BuildScripts/Editor/Build.cs
@@ -293,7 +293,7 @@ public static class Build
 #endif
                 break;
             case BuildTarget.WebGL:
-                PlayerSettings.SetManagedStrippingLevel(platform.BuildTargetGroup, ManagedStrippingLevel.Medium);
+                PlayerSettings.SetManagedStrippingLevel(platform.BuildTargetGroup, ManagedStrippingLevel.Low);
                 PlayerSettings.WebGL.decompressionFallback = true;
                 PlayerSettings.WebGL.compressionFormat     = WebGLCompressionFormat.Disabled; // Disable compression for FBInstant game
                 PlayerSettings.runInBackground             = false;
