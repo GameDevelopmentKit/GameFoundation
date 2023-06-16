0b1b71d36a6246a18bebc655cf1c0d74f7afbd69
Tuha <88423816+tuhamm@users.noreply.github.com>
2023-06-16 12:44:27 +0700

    Set exception to none

--- BuildScripts/Editor/Build.cs
+++ BuildScripts/Editor/Build.cs
@@ -309,7 +309,7 @@ public static class Build
                 PlayerSettings.runInBackground             = false;
                 PlayerSettings.WebGL.powerPreference       = WebGLPowerPreference.Default;
                 PlayerSettings.WebGL.dataCaching           = true;
-                PlayerSettings.WebGL.exceptionSupport      = WebGLExceptionSupport.ExplicitlyThrownExceptionsOnly;
+                PlayerSettings.WebGL.exceptionSupport      = WebGLExceptionSupport.None;
 #if UNITY_2022_1_OR_NEWER
                 PlayerSettings.WebGL.initialMemorySize = 64;
                 PlayerSettings.WebGL.memoryGrowthMode  = WebGLMemoryGrowthMode.Linear;
