82d19b449a9434cc3ba804aa32c49f006ba43e28
Tuha <88423816+tuhamm@users.noreply.github.com>
2023-06-17 15:08:19 +0700

    Change log to script only

--- BuildScripts/Editor/Build.cs
+++ BuildScripts/Editor/Build.cs
@@ -318,11 +318,11 @@ public static class Build
                 Console.WriteLine($"----------Setup build setting for FB_INSTANT_PRODUCTION platform----------");
                 PlayerSettings.WebGL.showDiagnostics = false;
                 
-                PlayerSettings.SetStackTraceLogType(LogType.Error, StackTraceLogType.None);
-                PlayerSettings.SetStackTraceLogType(LogType.Assert, StackTraceLogType.None);
-                PlayerSettings.SetStackTraceLogType(LogType.Warning, StackTraceLogType.None);
-                PlayerSettings.SetStackTraceLogType(LogType.Log, StackTraceLogType.None);
-                PlayerSettings.SetStackTraceLogType(LogType.Exception, StackTraceLogType.None);
+                PlayerSettings.SetStackTraceLogType(LogType.Error, StackTraceLogType.ScriptOnly);
+                PlayerSettings.SetStackTraceLogType(LogType.Assert, StackTraceLogType.ScriptOnly);
+                PlayerSettings.SetStackTraceLogType(LogType.Warning, StackTraceLogType.ScriptOnly);
+                PlayerSettings.SetStackTraceLogType(LogType.Log, StackTraceLogType.ScriptOnly);
+                PlayerSettings.SetStackTraceLogType(LogType.Exception, StackTraceLogType.ScriptOnly);
                 // PlayerSettings.WebGL.exceptionSupport = WebGLExceptionSupport.None;
 #else
                 PlayerSettings.WebGL.showDiagnostics = true;
