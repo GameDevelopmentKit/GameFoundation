d6320f15d4eef3f2f40564142ad4e7b8d965ccea
khai1412 <khai14122002123@gmail.com>
2023-06-22 15:19:31 +0700

    fix appflyer

--- BuildScripts/Editor/Build.cs
+++ BuildScripts/Editor/Build.cs
@@ -8,7 +8,6 @@ using UnityEditor.AddressableAssets.Build;
 using UnityEditor.AddressableAssets.Settings;
 using UnityEditor.Build;
 using UnityEditor.Build.Reporting;
-using UnityEditor.WebGL;
 using UnityEngine;
 
 // ------------------------------------------------------------------------
