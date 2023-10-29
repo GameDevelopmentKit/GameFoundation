5cd3d821d04608d01f2c1c9797ce4d9d1b110fde
Tuha <88423816+tuhamm@users.noreply.github.com>
2023-10-29 13:44:12 +0700

    Update datetime extension

--- Scripts/Utilities/Extension/DateTimeExtensions.cs
+++ Scripts/Utilities/Extension/DateTimeExtensions.cs
@@ -26,5 +26,18 @@ namespace GameFoundation.Scripts.Utilities.Extension
         }
 
         public static string ToFormatDateTime(this DateTime dateTime) { return $"{dateTime:dd/MM/yyyy}"; }
+        
+        public static int GetWeekIndexOfYear(this DateTime date)
+        {
+            var ci = System.Globalization.CultureInfo.CurrentCulture;
+            return ci.Calendar.GetWeekOfYear(date, ci.DateTimeFormat.CalendarWeekRule, ci.DateTimeFormat.FirstDayOfWeek);
+        }
+        
+        public static int GetWeekIndexFromStart(this DateTime date)
+        {
+            var span      = date - DateTime.MinValue;
+            var      weekIndex = (int)(span.TotalDays / 7);
+            return weekIndex;
+        }
     }
 }
