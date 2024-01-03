c0105566f8f445b547f6777b04d4fe04e4e69673
ninhnv <ninhnv15@gmail.com>
2024-01-03 11:16:18 +0700

    add sprite atlas utils

---
+++ Scripts/Utilities/SpriteAtlas/SpriteAtlasHandler.cs
@@ -0,0 +1,25 @@
+﻿namespace Utilities.SpriteAtlas
+{
+    using System;
+    using Cysharp.Threading.Tasks;
+    using GameFoundation.Scripts.AssetLibrary;
+    using UnityEngine;
+    using UnityEngine.U2D;
+    using Zenject;
+
+    public class SpriteAtlasHandler : MonoBehaviour
+    {
+        [Inject] private IGameAssets gameAssets;
+
+        void Awake() { SpriteAtlasManager.atlasRequested += this.AtlasRegistered; }
+
+        void OnDestroy() { SpriteAtlasManager.atlasRequested -= this.AtlasRegistered; }
+
+        async void AtlasRegistered(string atlasName, Action<SpriteAtlas> callBack)
+        {
+            // Addressable Name
+            var handle = await this.gameAssets.LoadAssetAsync<SpriteAtlas>(atlasName);
+            callBack(handle);
+        }
+    }
+}
