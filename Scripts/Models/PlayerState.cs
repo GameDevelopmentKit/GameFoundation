76375037c0f89c88d8912e155072c3d362a1c746
ninhnv15 <53180372+ninhnv15@users.noreply.github.com>
2024-01-27 18:39:37 +0700

    refactor models folder & remove DefaultPlayables folder

--- Scripts/Models/PlayerState.cs
+++
@@ -1,8 +0,0 @@
-namespace GameFoundation.Scripts.Models
-{
-    //TODO refactor this to interface
-    public class PlayerState
-    {
-        public PlayerData PlayerData { get; } = new PlayerData();
-    }
-}
