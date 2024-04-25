b09dfabe515e3d2946918a48f6347f353ee40e45
tuha <tuha263@gmail.com>
2024-04-25 10:17:39 +0700

    UPdate sound setting

--- Scripts/Models/GameFoundationLocalData.cs
+++ Scripts/Models/GameFoundationLocalData.cs
@@ -1,15 +1,15 @@
 namespace GameFoundation.Scripts.Models
 {
     using GameFoundation.Scripts.Interfaces;
-    using UniRx;
+    using R3;
 
     public class SoundSetting : ILocalData
     {
-        public BoolReactiveProperty  MasterVolume { get; set; } = new(true);
-        public BoolReactiveProperty  MuteMusic    { get; set; } = new(false);
-        public BoolReactiveProperty  MuteSound    { get; set; } = new(false);
-        public FloatReactiveProperty MusicValue   { get; set; } = new(1);
-        public FloatReactiveProperty SoundValue   { get; set; } = new(1);
+        public ReactiveProperty<bool>  MasterVolume { get; set; } = new(true);
+        public ReactiveProperty<bool>  MuteMusic    { get; set; } = new(false);
+        public ReactiveProperty<bool>  MuteSound    { get; set; } = new(false);
+        public ReactiveProperty<float> MusicValue   { get; set; } = new(1);
+        public ReactiveProperty<float> SoundValue   { get; set; } = new(1);
         
         public void Init()
         {
