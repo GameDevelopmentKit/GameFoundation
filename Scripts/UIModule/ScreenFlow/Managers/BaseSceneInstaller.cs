d630762d031468408a20af37ef9da9eafe643173
tuha <tuha263@gmail.com>
2024-05-04 20:44:24 +0700

    Fix complie bug

--- Scripts/UIModule/ScreenFlow/Managers/BaseSceneInstaller.cs
+++ Scripts/UIModule/ScreenFlow/Managers/BaseSceneInstaller.cs
@@ -58,8 +58,8 @@ namespace GameFoundation.Scripts.UIModule.ScreenFlow.Managers
                 }
 
                 EditorApplication.ExitPlaymode();
-#endif
             }
         }
+#endif
     }
 }
