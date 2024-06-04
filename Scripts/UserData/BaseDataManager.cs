d784eca259ba9769dd3a3f027997b7efa8f359d0
ninhnv15 <ninhnv15@gmail.com>
2024-06-05 00:07:55 +0700

    fix bug: inject and Initialize at the same time

--- Scripts/UserData/BaseDataManager.cs
+++ Scripts/UserData/BaseDataManager.cs
@@ -1,12 +1,15 @@
 namespace UserData
 {
+    using Cysharp.Threading.Tasks;
+
     public abstract class BaseDataManager<TData> where TData : class, IUserData, new()
     {
         protected TData Data { get; private set; }
 
-        public BaseDataManager(MasterDataManager masterDataManager) { this.Initialize(masterDataManager); }
-        private async void Initialize(MasterDataManager masterDataManager)
+        public BaseDataManager(MasterDataManager masterDataManager) { this.Initialize(masterDataManager).Forget(); }
+        private async UniTaskVoid Initialize(MasterDataManager masterDataManager)
         {
+            await  UniTask.Yield(PlayerLoopTiming.LastPostLateUpdate);
             await masterDataManager.DataReady.Task;
             this.Data = await masterDataManager.Get<TData>();
             this.OnDataLoaded(masterDataManager);
