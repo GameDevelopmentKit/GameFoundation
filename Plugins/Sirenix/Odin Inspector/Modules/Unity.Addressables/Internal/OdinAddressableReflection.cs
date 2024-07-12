895b1d50c8633615bcb22370e27603756e5cc932
tuha <tuha263@gmail.com>
2024-07-12 17:38:33 +0700

    Update new Odin version

---
+++ Plugins/Sirenix/Odin Inspector/Modules/Unity.Addressables/Internal/OdinAddressableReflection.cs
@@ -0,0 +1,23 @@
+//-----------------------------------------------------------------------
+// <copyright file="OdinAddressableReflection.cs" company="Sirenix ApS">
+// Copyright (c) Sirenix ApS. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System.Reflection;
+using UnityEditor.AddressableAssets.Settings;
+
+namespace Sirenix.OdinInspector.Modules.Addressables.Editor.Internal
+{
+	internal static class OdinAddressableReflection
+	{
+		public static FieldInfo AddressableAssetEntry_mGUID_Field;
+
+		static OdinAddressableReflection()
+		{
+			AddressableAssetEntry_mGUID_Field = typeof(AddressableAssetEntry).GetField("m_GUID", BindingFlags.Instance | BindingFlags.NonPublic);
+		}
+
+		internal static void EnsureConstructed() { }
+	}
+}
