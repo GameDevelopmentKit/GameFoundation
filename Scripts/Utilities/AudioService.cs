d55eed3715c9deb61283e7d5bf7f38df3cb260d9
hungdeptrai1311 <vumanhhung.bc2017@gmail.com>
2024-07-16 16:08:23 +0700

    custom audio service

--- Scripts/Utilities/AudioService.cs
+++ Scripts/Utilities/AudioService.cs
@@ -49,6 +49,7 @@
 
         private CompositeDisposable             compositeDisposable;
         private Dictionary<string, AudioSource> loopingSoundNameToSources = new();
+        private List<AudioSource>               oneShotSources            = new();
         private AudioSource                     MusicAudioSource;
 
         [Preserve]
@@ -114,8 +115,10 @@
             else
             {
                 audioSource.PlayOneShotSoundManaged(audioClip);
+                this.oneShotSources.Add(audioSource);
                 await UniTask.Delay(TimeSpan.FromSeconds(audioClip.length));
-                audioSource.Recycle();
+                this.oneShotSources.Remove(audioSource);
+                audioSource?.Recycle();
             }
         }
 
@@ -124,6 +127,11 @@
             SoundManager.StopAllLoopingSounds();
             SoundManager.StopAllNonLoopingSounds();
 
+            foreach (var audioSource in this.oneShotSources)
+            {
+                audioSource.gameObject.Recycle();
+            }
+            
             foreach (var audioSource in this.loopingSoundNameToSources.Values)
             {
                 audioSource.gameObject.Recycle();
