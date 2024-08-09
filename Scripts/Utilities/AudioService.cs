ee788a3201e6f5b4c87afc0ee2257d3c59917c7f
TungLV-GOG1071 <goldendarkness1071@gmail.com>
2024-08-09 14:32:56 +0700

    Add SetPlayListLoop in IAudioService

--- Scripts/Utilities/AudioService.cs
+++ Scripts/Utilities/AudioService.cs
@@ -15,21 +15,22 @@
 
     public interface IAudioService
     {
-        void PlaySound(string name, AudioSource sender);
-        void PlaySound(string name, bool isLoop = false, float volumeScale = 1f, float fadeSeconds = 1f, bool isAverage = false);
-        void StopAllSound();
-        void StopAll();
-        void PlayPlayList(string musicName, bool random = false, float volumeScale = 1f, float fadeSeconds = 1f, bool persist = false);
-        void PlayPlayList(AudioClip audioClip, bool random = false, float volumeScale = 1f, float fadeSeconds = 1f, bool persist = false);
-        void StopPlayList();
-        void SetPlayListTime(float time);
-        float GetPlayListTime();
-        void SetPlayListPitch(float pitch);
-        void PausePlayList();
-        void ResumePlayList();
-        void StopAllPlayList();
-        void PauseEverything();
-        void ResumeEverything();
+        void        PlaySound(string name, AudioSource sender);
+        void        PlaySound(string name, bool isLoop = false, float volumeScale = 1f, float fadeSeconds = 1f, bool isAverage = false);
+        void        StopAllSound();
+        void        StopAll();
+        void        PlayPlayList(string musicName, bool random = false, float volumeScale = 1f, float fadeSeconds = 1f, bool persist = false);
+        void        PlayPlayList(AudioClip audioClip, bool random = false, float volumeScale = 1f, float fadeSeconds = 1f, bool persist = false);
+        void        StopPlayList();
+        void        SetPlayListTime(float time);
+        float       GetPlayListTime();
+        void        SetPlayListPitch(float pitch);
+        public void SetPlayListLoop(bool isLoop);
+        void        PausePlayList();
+        void        ResumePlayList();
+        void        StopAllPlayList();
+        void        PauseEverything();
+        void        ResumeEverything();
     }
 
     public class AudioService : IAudioService, IInitializable, IDisposable
@@ -187,6 +188,12 @@
         {
             if (this.MusicAudioSource == null) return;
             this.MusicAudioSource.pitch = pitch;
+        }
+        
+        public void SetPlayListLoop(bool isLoop)
+        {
+            if (this.MusicAudioSource == null) return;
+            this.MusicAudioSource.loop = isLoop;
         }
 
         public void PausePlayList()
