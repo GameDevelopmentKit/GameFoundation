f72ec5f2d82a40f83cd31f9e8bf3eefcce22a9a2
chuongdp <chuongdp161@gmail.com>
2025-02-11 15:53:08 +0700

    change TextMeshProUGUI to use TMP_Text in BaseTextMeshPro

--- Scripts/Utilities/Utils/BaseTextMeshPro.cs
+++ Scripts/Utilities/Utils/BaseTextMeshPro.cs
@@ -7,13 +7,13 @@
     [DisallowMultipleComponent]
     public class BaseTextMeshPro : MonoBehaviour
     {
-        [SerializeField] private TextMeshProUGUI txtText;
-        private                  string          lastKey;
-        private                  TMP_FontAsset   currentFont;
+        [SerializeField] private TMP_Text      txtText;
+        private                  string        lastKey;
+        private                  TMP_FontAsset currentFont;
 
         private void Awake()
         {
-            this.txtText     ??= this.GetComponent<TextMeshProUGUI>();
+            this.txtText     ??= this.GetComponent<TMP_Text>();
             this.currentFont =   this.txtText.font;
             this.SetTextWithLocalization(this.txtText.text);
             LocalizationService.Instance.OnLanguageChange += this.OnLanguageChange;
@@ -23,7 +23,7 @@
 
         private async void SetTextWithLocalization(string key, Color colorCode = default)
         {
-            this.txtText      ??= this.GetComponent<TextMeshProUGUI>();
+            this.txtText      ??= this.GetComponent<TMP_Text>();
             this.txtText.text =   LocalizationService.Instance.GetTextWithKey(key);
 
             if (colorCode != default)
@@ -36,7 +36,7 @@
             //TODO change font with language
 
             var font = await LocalizationService.Instance.GetFontAsset();
-            
+
             if (font == null) return;
             this.txtText.font = LocalizationManager.CurrentLanguage == "English" ? this.currentFont : font;
         }
