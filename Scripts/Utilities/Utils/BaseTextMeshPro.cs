1a908078241ce0631d631fc54da747296825fb88
chuongdp <chuongdp161@gmail.com>
2025-03-19 09:16:02 +0700

    update BaseTextMeshPro with material

--- Scripts/Utilities/Utils/BaseTextMeshPro.cs
+++ Scripts/Utilities/Utils/BaseTextMeshPro.cs
@@ -1,5 +1,8 @@
 ﻿namespace GameFoundation.Scripts.Utilities.Utils
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
     using I2.Loc;
     using TMPro;
     using UnityEngine;
@@ -7,14 +10,18 @@
     [DisallowMultipleComponent]
     public class BaseTextMeshPro : MonoBehaviour
     {
-        [SerializeField] private TMP_Text      txtText;
-        private                  string        lastKey;
-        private                  TMP_FontAsset currentFont;
+        [SerializeField] private TMP_Text                 txtText;
+        [SerializeField] private List<MaterialDictionary> listMaterial = new();
+
+        private string        lastKey;
+        private TMP_FontAsset currentFont;
+        private Material      defaultMat;
 
         private void Awake()
         {
             this.txtText     ??= this.GetComponent<TMP_Text>();
             this.currentFont =   this.txtText.font;
+            this.defaultMat  =   this.txtText.fontSharedMaterial;
             this.SetTextWithLocalization(this.txtText.text);
             LocalizationService.Instance.OnLanguageChange += this.OnLanguageChange;
         }
@@ -23,8 +30,27 @@
 
         private async void SetTextWithLocalization(string key, Color colorCode = default)
         {
-            this.txtText      ??= this.GetComponent<TMP_Text>();
-            this.txtText.text =   LocalizationService.Instance.GetTextWithKey(key);
+            this.txtText ??= this.GetComponent<TMP_Text>();
+            var font = await LocalizationService.Instance.GetFontAsset();
+
+            if (font != null)
+            {
+                this.txtText.font = LocalizationManager.CurrentLanguage == "English" ? this.currentFont : font;
+            }
+            else
+            {
+                Debug.LogError("Font is null");
+            }
+
+            if (this.listMaterial.Count > 0)
+            {
+                var mat = this.listMaterial.FirstOrDefault(o => o.key == LocalizationManager.CurrentLanguage);
+
+                this.txtText.fontSharedMaterial = (mat == null || mat.value == null) ? this.defaultMat : mat.value;
+            }
+
+            // put set text after setting font/materials
+            this.txtText.text = LocalizationService.Instance.GetTextWithKey(key);
 
             if (colorCode != default)
             {
@@ -32,13 +58,13 @@
             }
 
             this.lastKey = key;
-
-            //TODO change font with language
-
-            var font = await LocalizationService.Instance.GetFontAsset();
-
-            if (font == null) return;
-            this.txtText.font = LocalizationManager.CurrentLanguage == "English" ? this.currentFont : font;
         }
     }
+
+    [Serializable]
+    public class MaterialDictionary
+    {
+        public string   key;
+        public Material value;
+    } 
 }
