618845ac58442d02d9a0524e0612d4223e63f76e
mike <duymaibui2k@gmail.com>
2025-07-17 11:11:13 +0700

    Revert "cloud save"
    This reverts commit 0b3e8daaadbfeb5fc0328ceee1f928a2664dc8ea.

--- Scripts/GameFoundationVContainer.cs
+++ Scripts/GameFoundationVContainer.cs
@@ -4,7 +4,6 @@
 #nullable enable
 namespace GameFoundation.Scripts
 {
-    using System;
     using GameFoundation.BlueprintFlow;
     using GameFoundation.DI;
     using GameFoundation.Scripts.AssetLibrary;
@@ -16,7 +15,6 @@ namespace GameFoundation.Scripts
     using GameFoundation.UIModule.UIModule;
     using GameFoundation.Utilities.ApplicationServices;
     using GameFoundation.Utilities.GameQueueAction;
-    using TheOne.Extensions;
     using TheOne.Logging.DI;
     using UnityEngine;
     using VContainer;
@@ -43,7 +41,6 @@ namespace GameFoundation.Scripts
             builder.Register<AudioService>(Lifetime.Singleton).AsImplementedInterfaces();
             builder.Register<HandleLocalUserDataServices>(Lifetime.Singleton).AsImplementedInterfaces();
             builder.Register<LoadImageHelper>(Lifetime.Singleton);
-            builder.Register(typeof(ICloudUserDataService).GetSingleDerivedType(), Lifetime.Singleton).As<ICloudUserDataService>();
 
             builder.DeclareSignal<UserDataLoadedSignal>();
         }
--- Scripts/Utilities/UserData/CloudData/ICloudData.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace GameFoundation.Scripts.Utilities.UserData
-{
-    using GameFoundation.Scripts.Interfaces;
-
-    public interface ICloudData : ILocalData
-    {
-    }
-}
