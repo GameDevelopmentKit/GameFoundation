536d3a51ff28d06e89d3d433a0160b2e99f7912c
ninhnv15 <ninhnv15@gmail.com>
2025-12-09 16:09:59 +0700

    remove build script

--- Scripts/Utilities/GameInfoMono.cs
+++ Scripts/Utilities/GameInfoMono.cs
@@ -1,6 +1,5 @@
 ﻿namespace GameFoundation.Scripts.Utilities
 {
-    using GameFoundation.BuildScripts.Runtime;
     using TMPro;
     using UnityEngine;
 
@@ -12,7 +11,6 @@
         private void Start()
         {
             this.gameInfoText      = this.GetComponent<TextMeshProUGUI>();
-            this.gameInfoText.text = GameVersion.FullInfo;
 #if !DEVELOPMENT_BUILD && !UNITY_EDITOR
             this.gameObject.SetActive(false);
 #endif
