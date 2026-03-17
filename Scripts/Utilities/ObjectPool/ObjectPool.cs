225cd072024dfc7964c31752543ff80112736ac9
CuongND <137050365+dyCuong03@users.noreply.github.com>
2026-03-17 14:29:05 +0700

    Implement: spawn from object pooling can set worldPositionStays when … (#193)
    * Implement: spawn from object pooling can set worldPositionStays when parent set
    
    * Remove unuse code
    
    ---------
    
    Co-authored-by: CuongND <cuongnd@archergame.mobi>

--- Scripts/Utilities/ObjectPool/ObjectPool.cs
+++ Scripts/Utilities/ObjectPool/ObjectPool.cs
@@ -9,7 +9,7 @@ namespace GameFoundation.Scripts.Utilities.ObjectPool
         public List<GameObject> pooledObjects = new List<GameObject>();
 
         private bool isDestroying;
-        public GameObject Spawn(Transform parent, Vector3 position, Quaternion rotation)
+        public GameObject Spawn(Transform? parent = null, Vector3 position = default, Quaternion rotation = default, bool spawnInWorldSpace = true)
         {
             GameObject obj;
             if (this.pooledObjects.Count == 0)
@@ -25,13 +25,12 @@ namespace GameFoundation.Scripts.Utilities.ObjectPool
 
                 var transformObj = obj.transform;
                 transformObj.SetLocalPositionAndRotation(position, rotation);
-                transformObj.localRotation = rotation;
                 obj.SetActive(true);
             }
 
             if (!ReferenceEquals(parent, null) && parent != obj.transform.parent)
             {
-                obj.transform.SetParent(parent);
+                obj.transform.SetParent(parent, spawnInWorldSpace);
             }
 
             // this.spawnedObjects.Add(obj);
@@ -45,7 +44,7 @@ namespace GameFoundation.Scripts.Utilities.ObjectPool
             // this.spawnedObjects.Remove(obj);
             obj.SetActive(false);
             if (!this.isDestroying && obj.transform.parent != this.transform)
-                obj.transform.SetParent(this.transform);
+                obj.transform.SetParent(this.transform, false);
         }
 
         public void CleanUpPooled()
