6f2e1a887d82444925eaaeb6a2bf94de9c94e096
hoanghai145 <hoangthanhhai1308@gmail.com>
2026-04-23 20:54:54 +0700

    update

--- Scripts/UIModule/ScreenFlow/Managers/RootUICanvas.cs
+++ Scripts/UIModule/ScreenFlow/Managers/RootUICanvas.cs
@@ -11,12 +11,13 @@ namespace GameFoundation.Scripts.UIModule.ScreenFlow.Managers
         [SerializeField] private Transform rootUIClosedTransform;
         [SerializeField] private Transform rootUIOverlayTransform;
         [SerializeField] private Camera    uiCamera;
+        [SerializeField] private Canvas    uiCanvas;
 
         public Camera    UICamera               => this.uiCamera;
         public Transform RootUIShowTransform    => this.rootUIShowTransform;
         public Transform RootUIClosedTransform  => this.rootUIClosedTransform;
         public Transform RootUIOverlayTransform => this.rootUIOverlayTransform;
-
+        public Canvas    Canvas                 => this.uiCanvas;
 
         private void Awake()
         {
@@ -25,6 +26,7 @@ namespace GameFoundation.Scripts.UIModule.ScreenFlow.Managers
             this.rootUIClosedTransform ??= this.transform;
 
             this.rootUIOverlayTransform ??= this.transform;
+            this.uiCamera               ??= this.GetComponentInChildren<Camera>(true);
         }
     }
 }
