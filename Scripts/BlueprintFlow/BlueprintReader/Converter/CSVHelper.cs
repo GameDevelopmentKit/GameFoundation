d937ba7de43449cb67e679e12cfa3b70c8d4b6c6
hoanghai145 <hoangthanhhai1308@gmail.com>
2026-06-12 09:28:31 +0700

    update csv helper

--- Scripts/BlueprintFlow/BlueprintReader/Converter/CSVHelper.cs
+++ Scripts/BlueprintFlow/BlueprintReader/Converter/CSVHelper.cs
@@ -3,6 +3,7 @@ namespace BlueprintFlow.BlueprintReader.Converter
     using System;
     using System.Collections.Generic;
     using System.Linq;
+    using System.Linq.Expressions;
     using System.Reflection;
     using BlueprintFlow.BlueprintReader.Converter.TypeConversion;
     using Sylvan.Data.Csv;
@@ -71,24 +72,113 @@ namespace BlueprintFlow.BlueprintReader.Converter
         /// <summary>
         ///     Utility to get all member infos from a class map
         /// </summary>
+        // public static List<MemberInfo> GetAllFieldAndProperties(this Type typeInfo)
+        // {
+        //     if (MemberInfosCache.TryGetValue(typeInfo, out var results)) return results;
+        //
+        //     results = typeInfo.GetFields().Select(fieldInfo => new MemberInfo
+        //     {
+        //         MemberName = fieldInfo.Name, MemberType   = fieldInfo.FieldType,
+        //         SetValue   = fieldInfo.SetValue, GetValue = fieldInfo.GetValue,
+        //         IsDefined  = type => fieldInfo.IsDefined(type, false)
+        //     }).ToList();
+        //
+        //     results.AddRange(typeInfo.GetProperties().Select(propertyInfo => new MemberInfo
+        //     {
+        //         MemberName = propertyInfo.Name, MemberType   = propertyInfo.PropertyType,
+        //         SetValue   = propertyInfo.SetValue, GetValue = propertyInfo.GetValue,
+        //         IsDefined  = type => propertyInfo.IsDefined(type, false)
+        //     }));
+        //     MemberInfosCache[typeInfo] = results;
+        //     return results;
+        // }
         public static List<MemberInfo> GetAllFieldAndProperties(this Type typeInfo)
         {
             if (MemberInfosCache.TryGetValue(typeInfo, out var results)) return results;
 
-            results = typeInfo.GetFields().Select(fieldInfo => new MemberInfo
+            results = typeInfo.GetFields().Select(fieldInfo =>
             {
-                MemberName = fieldInfo.Name, MemberType   = fieldInfo.FieldType,
-                SetValue   = fieldInfo.SetValue, GetValue = fieldInfo.GetValue,
-                IsDefined  = type => fieldInfo.IsDefined(type, false)
+                var objParam = Expression.Parameter(typeof(object), "obj");
+                var valParam = Expression.Parameter(typeof(object), "val");
+
+                Expression instanceForGet = typeInfo.IsValueType
+                    ? Expression.Unbox(objParam, typeInfo)
+                    : Expression.Convert(objParam, typeInfo);
+
+                Expression instanceForSet = typeInfo.IsValueType
+                    ? Expression.Unbox(objParam, typeInfo)
+                    : Expression.Convert(objParam, typeInfo);
+
+                var getter = Expression.Lambda<Func<object, object>>(
+                    Expression.Convert(
+                        Expression.Field(instanceForGet, fieldInfo),
+                        typeof(object)),
+                    objParam
+                ).Compile();
+
+                var setter = Expression.Lambda<Action<object, object>>(
+                    Expression.Assign(
+                        Expression.Field(instanceForSet, fieldInfo),
+                        Expression.Convert(valParam, fieldInfo.FieldType)),
+                    objParam, valParam
+                ).Compile();
+
+                return new MemberInfo
+                {
+                    MemberName = fieldInfo.Name,
+                    MemberType = fieldInfo.FieldType,
+                    SetValue   = setter,
+                    GetValue   = getter,
+                    IsDefined  = type => fieldInfo.IsDefined(type, false)
+                };
             }).ToList();
 
-            results.AddRange(typeInfo.GetProperties().Select(propertyInfo => new MemberInfo
-            {
-                MemberName = propertyInfo.Name, MemberType   = propertyInfo.PropertyType,
-                SetValue   = propertyInfo.SetValue, GetValue = propertyInfo.GetValue,
-                IsDefined  = type => propertyInfo.IsDefined(type, false)
-            }));
+            results.AddRange(typeInfo.GetProperties()
+                .Where(p => p.GetIndexParameters().Length == 0)
+                .Select(propertyInfo =>
+                {
+                    var objParam = Expression.Parameter(typeof(object), "obj");
+                    var valParam = Expression.Parameter(typeof(object), "val");
+
+                    var instanceForGet = typeInfo.IsValueType
+                        ? Expression.Unbox(objParam, typeInfo)
+                        : Expression.Convert(objParam, typeInfo);
+
+                    var getter = Expression.Lambda<Func<object, object>>(
+                        Expression.Convert(
+                            Expression.Property(instanceForGet, propertyInfo),
+                            typeof(object)),
+                        objParam
+                    ).Compile();
+
+                    Action<object, object> setter = null;
+
+                    if (propertyInfo.CanWrite && propertyInfo.SetMethod != null)
+                    {
+                        var instanceForSet = typeInfo.IsValueType
+                            ? Expression.Unbox(objParam, typeInfo)
+                            : Expression.Convert(objParam, typeInfo);
+
+                        setter = Expression.Lambda<Action<object, object>>(
+                            Expression.Assign(
+                                Expression.Property(instanceForSet, propertyInfo),
+                                Expression.Convert(valParam, propertyInfo.PropertyType)),
+                            objParam, valParam
+                        ).Compile();
+                    }
+
+                    return new MemberInfo
+                    {
+                        MemberName = propertyInfo.Name,
+                        MemberType = propertyInfo.PropertyType,
+                        SetValue   = setter,
+                        GetValue   = getter,
+                        IsDefined  = type => propertyInfo.IsDefined(type, false)
+                    };
+                }));
+
             MemberInfosCache[typeInfo] = results;
+
             return results;
         }
 
