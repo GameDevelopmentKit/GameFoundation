461a4a8c3f53e2dfaa99f60445de168a1ed36ad3
ninhnv <ninhnv15@gmail.com>
2026-07-16 01:54:52 +0700

    tweak TextLayoutElement.cs

--- Scripts/UIModule/Utilities/UILayoutElement/TextLayoutElement.cs
+++ Scripts/UIModule/Utilities/UILayoutElement/TextLayoutElement.cs
@@ -40,13 +40,15 @@ namespace GameFoundation.Scripts.UIModule.Utilities.UILayoutElement
 
         protected override void OnEnable()
         {
-            this.isValid = this.text != null && this.rectTransform != null;
+            this.UpdateValidState();
+            TMPro_EventManager.TEXT_CHANGED_EVENT.Add(this.OnTextChanged);
             base.OnEnable();
             this.SetDirty();
         }
 
         protected override void OnDisable()
         {
+            TMPro_EventManager.TEXT_CHANGED_EVENT.Remove(this.OnTextChanged);
             this.m_Tracker.Clear();
             LayoutRebuilder.MarkLayoutForRebuild(this.rectTransform);
             base.OnDisable();
@@ -60,10 +62,17 @@ namespace GameFoundation.Scripts.UIModule.Utilities.UILayoutElement
 #if UNITY_EDITOR
         protected override void OnValidate()
         {
-            this.SetDirty(); 
+            this.UpdateValidState();
+            this.SetDirty();
         }
 
 #endif
+
+        public void RebuildLayout()
+        {
+            this.SetLayoutHorizontal();
+            this.SetLayoutVertical();
+        }
   
         protected void SetDirty()
         {
@@ -82,9 +91,10 @@ namespace GameFoundation.Scripts.UIModule.Utilities.UILayoutElement
         }
 
         public void SetLayoutHorizontal() {
+            this.UpdateValidState();
             this.m_Tracker.Clear();
             if (this.isValid && this.fitWidth) {
-                var preferredValuesX = this.text.GetPreferredValues().x;
+                var preferredValuesX = this.text.GetPreferredValues(this.text.text).x;
                 if(Mathf.Approximately(preferredValuesX,this.rectTransform.sizeDelta.x)) return;
                 this.m_Tracker.Add(this, this.rectTransform, DrivenTransformProperties.SizeDeltaX);
                 var maxX = this.maxValue.x > 0 ? this.maxValue.x : preferredValuesX;
@@ -93,12 +103,44 @@ namespace GameFoundation.Scripts.UIModule.Utilities.UILayoutElement
         }
 
         public void SetLayoutVertical() {
+            this.UpdateValidState();
             if (this.isValid && this.fitHeight) {
                 this.m_Tracker.Add(this,this.rectTransform, DrivenTransformProperties.SizeDeltaY);
-                var preferredValues = this.text.GetPreferredValues();
+                var preferredValues = this.GetPreferredValuesForCurrentWidth();
                 var maxY            = this.maxValue.y > 0 ? this.maxValue.y : preferredValues.y;
                 this.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, Mathf.Clamp(preferredValues.y, this.minValue.y, maxY));
             }
+        }
+
+        private void OnTextChanged(UnityEngine.Object changedObject)
+        {
+            if (changedObject == this.text)
+                this.SetDirty();
+        }
+
+        private void UpdateValidState()
+        {
+            this.isValid = this.text != null && this.rectTransform != null;
+        }
+
+        private Vector2 GetPreferredValuesForCurrentWidth()
+        {
+            var width = this.GetTextWidthForHeightCalculation();
+            return width > 0
+                ? this.text.GetPreferredValues(this.text.text, width, 0)
+                : this.text.GetPreferredValues(this.text.text);
+        }
+
+        private float GetTextWidthForHeightCalculation()
+        {
+            var textRect = this.text.rectTransform;
+            if (textRect != null && textRect.rect.width > 0)
+                return textRect.rect.width;
+
+            if (this.rectTransform.rect.width > 0)
+                return this.rectTransform.rect.width;
+
+            return this.rectTransform.sizeDelta.x > 0 ? this.rectTransform.sizeDelta.x : 0;
         }
     }
 }
